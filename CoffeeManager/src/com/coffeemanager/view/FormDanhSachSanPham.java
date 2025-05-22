5609a6170fcf8fe33a51581f18ade3d6c56b9290
Nguyen Tan Duy <ntanduy1122@gmail.com>
2025-05-23 00:15:11 +0700

    FormDanhSachSanPham hoàn thành

--- CoffeeManager/src/com/coffeemanager/view/FormDanhSachSanPham.java
+++ CoffeeManager/src/com/coffeemanager/view/FormDanhSachSanPham.java
@@ -60,7 +60,6 @@ public class FormDanhSachSanPham extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -230,15 +229,6 @@ public class FormDanhSachSanPham extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem1);
-
-        jMenuItem2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jMenuItem2.setText("Bảng Lương");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
 
@@ -430,18 +420,6 @@ public class FormDanhSachSanPham extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        if ("Quản lý".equalsIgnoreCase(chucVu)) {
-            new FormBangLuong(chucVu).setVisible(true);
-        } else if ("Nhân viên".equalsIgnoreCase(chucVu)) {
-            new FormBangLuong(chucVu).setVisible(true);
-        } else {
-            JOptionPane.showMessageDialog(this, "❌ Vai trò không hợp lệ!", "Lỗi", JOptionPane.ERROR_MESSAGE);
-        }
-        this.dispose(); // Đóng form hiện tại
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_suaSanPham;
@@ -455,7 +433,6 @@ public class FormDanhSachSanPham extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane2;
